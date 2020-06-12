3f1ff85783b137ff7a1a950c7c5d9e59b84c4afc
jayrugman <jasonrhardman@gmail.com>
2020-06-12 00:04:23 -0600

    added Hacker.cs, as well added that to the game.unity scene

---
+++ Terminal Hacker/Assets/Scenes/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu() {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("WARNING:\nBeware, using this program is illegal  in all 50 states!");
+        Terminal.WriteLine("With that, what would you like to hack?");
+        Terminal.WriteLine(" 1- The Kitchen");
+        Terminal.WriteLine(" 2- The Sound System");
+        Terminal.WriteLine(" 3- The Smart-Home Core System");
+        Terminal.WriteLine("\nMake a selection:");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
