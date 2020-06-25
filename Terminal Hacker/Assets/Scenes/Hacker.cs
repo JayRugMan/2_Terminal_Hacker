87c90dd64f1332f015acac9e3d2e393dd30e80b1
jayrugman <jasonrhardman@gmail.com>
2020-06-25 00:42:49 -0600

    refactored code and added additional menu behavior, as well as game-state variables

--- Terminal Hacker/Assets/Scenes/Hacker.cs
+++ Terminal Hacker/Assets/Scenes/Hacker.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+
+    // Game State
+    int level;
+    enum screen { MainMenu, Password, Win }
+    screen currentScreen;
+
     // Start is called before the first frame update
     void Start() {
         string playerName = "Jason";
         string greeting = "Hello " + playerName;
         ShowMainMenu(greeting);
     }
+
     void ShowMainMenu(string tagline) {
+        currentScreen = screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine(tagline);
         Terminal.WriteLine("\nBeware, using this program is illegal  in all 50 states!");
@@ -19,12 +27,27 @@ public class Hacker : MonoBehaviour {
         Terminal.WriteLine(" 3- The Smart-Home Core System");
         Terminal.WriteLine("\nMake a selection:");
     }
+
 	void OnUserInput(string input) {
         string playerName = "Jason";
-		if (input == "menu") {
+		if (input == "menu") {  // typing "menu" will always get you to main menu
             print("Returing to Main Menu.");
             string greeting = "Sure, " + playerName + ", have another go.";
             ShowMainMenu(greeting);
+        }
+        else if (currentScreen == screen.MainMenu) {
+            RunMainMenu(input);
+        }
+	}
+
+    void RunMainMenu(string input) {
+        if (input == "1") {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2") {
+            level = 2;
+            StartGame();
         }
         else if (input == "1234") {
             print("That was a secret");
@@ -34,5 +57,11 @@ public class Hacker : MonoBehaviour {
         else {
             Terminal.WriteLine("Select from the listed options:");
         }
-	}
+    }
+    
+    void StartGame() {
+        currentScreen = screen.Password;
+        Terminal.WriteLine("You have chosed level " + level);
+        Terminal.WriteLine("Password: ");
+    }
 }
